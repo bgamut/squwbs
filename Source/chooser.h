46f02c90346ce108ccc248d3ee217352f4b3168f
bgamut <bgamut@gmail.com>
2025-10-01 16:42:32 +0900

    Update chooser.h

--- Source/chooser.h
+++ Source/chooser.h
@@ -17,47 +17,119 @@
   ==============================================================================
 */
 
-#pragma once
-
-//[Headers]     -- You can add your own extra header files here --
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "ThreeBandEQ.h"
-#include "FourBandEQ.h"
+//[Headers] You can add your own extra header files here...
 //[/Headers]
 
+#include "chooser.h"
 
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
 
 //==============================================================================
-/**
-                                                                    //[Comments]
-    An auto-generated component, created by the Projucer.
-
-    Describe your class and how it works here!
-                                                                    //[/Comments]
-*/
-class chooser  : public Component
-	             //,public AudioAppComponent
+chooser::chooser ():
+changed(false)
 {
-public:
-    //==============================================================================
-    chooser ();
-    ~chooser();
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
 
-    //==============================================================================
-    //[UserMethods]     -- You can add your own custom methods in this section.
-    //[/UserMethods]
-	void loadFile();
-	AudioFormatManager formatManager;
-	ScopedPointer<AudioFormatReaderSource> readerSource;
-	AudioFormatReader* reader;
-	ThreeBandEQ threeband;
-	FourBandEQ fourband;
-	AudioSampleBuffer fileBuffer;
-    bool changed;
-private:
- 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (chooser)
-};
 
-//[EndFile] You can add extra defines here...
-//[/EndFile]
+    //[UserPreSize]
+    //[/UserPreSize]
+    //setSize (600, 400);
+	formatManager.registerBasicFormats();
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
+}
+
+chooser::~chooser()
+{
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
+}
+
+void chooser::loadFile()
+{
+    /*
+	FileChooser myChooser("Please select the reference file you want to load.", File::getSpecialLocation(File::userHomeDirectory), "*");
+	myChooser.browseForFileToOpen();
+	File r(myChooser.getResult());
+	if (r.exists()) {
+		ScopedPointer<AudioFormatReader> reader(formatManager.createReaderFor(r));
+		if (reader != nullptr) {
+			int testLength = reader->lengthInSamples;
+			//threeband.setSampleRate(reader->sampleRate);
+			fourband.setSampleRate(reader->sampleRate);
+			fileBuffer.setSize(2, testLength);
+			reader->read(&fileBuffer, 0, testLength, 0, true, true);
+			int numInputChannels = fileBuffer.getNumChannels();
+			AudioBuffer<float> analysisBuffer(2, testLength);
+			AudioSourceChannelInfo channelInfo(analysisBuffer);
+			float* left = new float[testLength];
+			float* right = new float[testLength];
+			if (numInputChannels == 1) {
+				for (int index = 0; index < testLength; index++) {
+					left[index] = fileBuffer.getSample(0, index);
+					right[index] = fileBuffer.getSample(0, index);
+				}
+			}
+			else {
+				for (int index = 0; index < testLength; index++) {
+					left[index] = fileBuffer.getSample(0, index);
+					right[index] = fileBuffer.getSample(1, index);
+				}
+			}
+
+			//threeband.runStats(left, right, testLength);
+			fourband.runStats(left, right, testLength);
+			std::cerr<< std::to_string(fourband.midtoprms) << "," << std::to_string(fourband.midhighrms) << "," << std::to_string(fourband.midmidrms) << "," << std::to_string(fourband.midlowrms) << "," << std::to_string(fourband.sidetoprms) << "," << std::to_string(fourband.sidehighrms) << "," << std::to_string(fourband.sidemidrms) << "," << std::to_string(fourband.sidelowrms) << "," << std::to_string(fourband.midtopsd) << "," << std::to_string(fourband.midhighsd) << "," << std::to_string(fourband.midmidsd) << "," << std::to_string(fourband.midlowsd) << "," << std::to_string(fourband.sidetopsd) << "," << std::to_string(fourband.sidehighsd) << "," << std::to_string(fourband.sidemidsd) << "," << std::to_string(fourband.sidelowsd) << std::endl;
+			String MyFilePath = "C:\\Users\\bernard\\Desktop\\musicData.txt";
+			File w(MyFilePath);
+
+			w.appendText(std::to_string(fourband.midtoprms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midhighrms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midmidrms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midlowrms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidetoprms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidehighrms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidemidrms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidelowrms));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midtopsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midhighsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midmidsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.midlowsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidetopsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidehighsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidemidsd));
+			w.appendText(",");
+			w.appendText(std::to_string(fourband.sidelowsd));
+			//w.appendText(",");
+			w.create();
+			delete[] left;
+			delete[] right;
+            changed=true;
+		}
+	}
+     */
+}
+
+
